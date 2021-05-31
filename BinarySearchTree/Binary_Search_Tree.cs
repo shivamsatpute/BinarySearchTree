2c78004ac698cbfbcfbb7f68cdfb2faa11b65279
shivam satpute <shivam897satpute@gmail.com>
2021-05-31 22:13:07 +0530

    [shivam]add: Create Binary Search Tree And Insert 56 ,30 and 70

---
+++ BinarySearchTree/Binary_Search_Tree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Binary_Search_Tree<T> where T : IComparable<T>
+    {
+        public T NodeData { get; set; }
+        public Binary_Search_Tree<T> LeftTree { get; set; }
+        public Binary_Search_Tree<T> RightTree { get; set; }
+        public Binary_Search_Tree(T nodeData) 
+        {
+            NodeData = nodeData;
+            LeftTree = null;
+            RightTree = null;
+        }
+        int leftCount = 0; 
+        int rightCount = 0;
+        bool result = false;
+        public void Insert(T item)
+        {
+            try
+            {
+                T CurrentNodeValue = this.NodeData;
+                if (CurrentNodeValue.CompareTo(item) > 0) 
+                {
+                    if (this.LeftTree == null)
+                    {
+                        this.LeftTree = new Binary_Search_Tree<T>(item);
+                    }
+                    else
+                    {
+                        this.LeftTree.Insert(item);
+                    }
+                }
+                else
+                {
+                    if (this.RightTree == null) 
+                    {
+                        this.RightTree = new Binary_Search_Tree<T>(item);
+                    }
+                    else
+                    {
+                        this.RightTree.Insert(item);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+        public void Display() 
+        {
+            try
+            {
+                if (this.LeftTree != null)
+                {
+                    this.leftCount++;
+                    this.LeftTree.Display(); 
+                }
+                Console.WriteLine(this.NodeData.ToString());
+                if (this.RightTree != null)
+                {
+                    this.rightCount++;
+                    this.RightTree.Display(); 
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("BinarySearchTree");
+            Binary_Search_Tree<int> bst = new Binary_Search_Tree<int>(56); 
+            bst.Insert(30); 
+            bst.Insert(70);
+
+            bst.Display(); 
+            Console.WriteLine("         56        ---> Root \n");
+            Console.WriteLine("     30     70");
+
+            Console.ReadLine();
+        }
+    }
+    
+}
